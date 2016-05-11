2f1d6659237325609490eb5832345da1002b6ae6
tiamalist <tiamalist@gmail.com>
2016-05-11 23:06:01 +0300

    Init-13

--- CC_3/Program.cs
+++ CC_3/Program.cs
@@ -8,17 +8,19 @@ namespace CC_3
 {
     class Program
     {
-
+        public static string NewCC = "";
+        public static bool ValueBoolPerevirka;
+    
         static void Main(string[] args)
         {
             Console.WriteLine("Введiть номер кредитної карти:");
-           string NCard = Console.ReadLine();
+            string NCard = Console.ReadLine();
             Console.WriteLine("Вендор карти");
-           GetCreditCardVendor(NCard);
+            GetCreditCardVendor(NCard);
             Console.WriteLine("Перевiрка валiдностi номера карти");
-           IsCreditCardNumberValid(NCard);
+            IsCreditCardNumberValid(NCard);
             Console.WriteLine("Пошук наступного номера карти");
-           GenerateNextCreditCardNumber(NCard);                      
+            GenerateNextCreditCardNumber(NCard);
             Console.WriteLine("End");
             Console.ReadLine();
         }
@@ -34,45 +36,45 @@ namespace CC_3
                     KS = KS + s[i];                 // 
                 }                                   //    
             }                                       //
-            
+
             switch (s[0])
             {
                 case '4':
-                    
+
                     if (KS.Length == 13)
                     {
                         Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                         Console.WriteLine("VISA");
                     }
                     else if (KS.Length == 16)
-                    {
+                    {                    
                         Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                         Console.WriteLine("VISA");
                     }
                     else if (KS.Length == 19)
-                    {
+                    {                   
                         Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                         Console.WriteLine("VISA");
-                    }
+                    }    
                     else
                     {
                         Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
                         return s;
-                    }
+                    }               
                     break;
                 case '3':
                     switch (s[1])
                     {
-                        case '4':                            
+                        case '4':
                             if (KS.Length == 15)
-                            {
+                            {                            
                                 Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                                 Console.WriteLine("American Express");
                             }
                             else
                             {
-                                Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");                                
-                                return s;
+                                Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
+                                return s;                               
                             }
                             break;
                         case '7':
@@ -84,19 +86,19 @@ namespace CC_3
                             else
                             {
                                 Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
-                                return s;
+                                return s;                              
                             }
                             break;
-                        case '5':                            
+                        case '5':
                             if (KS.Length == 16)
-                            {
+                            {                             
                                 Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                                 Console.WriteLine("JCB"); //JCB
                             }
                             else
-                            {
+                            {                                
                                 Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
-                                return s;
+                                return s;                             
                             }
                             break;
                         default:
@@ -108,21 +110,21 @@ namespace CC_3
                     switch (s[1])
                     {
                         // Вендор MasterCard
-                        case '1':                            
+                        case '1':
                             if (KS.Length == 16)
-                            {
+                            {                             
                                 Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                                 Console.WriteLine("MasterCard");
                             }
                             else
                             {
                                 Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
-                                return s;
+                                return s;                                
                             }
                             break;
                         case '2':
                             if (KS.Length == 16)
-                            {
+                            {                            
                                 Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                                 Console.WriteLine("MasterCard");
                             }
@@ -235,7 +237,7 @@ namespace CC_3
                     }
                     break;
                 case '6':
-                    if (KS.Length >=12 && KS.Length <= 19)
+                    if (KS.Length >= 12 && KS.Length <= 19)
                     {
                         Console.WriteLine("Номер вiдповiдає кiлькостi символiв");
                         Console.WriteLine("Maestro");
@@ -243,68 +245,66 @@ namespace CC_3
                     else
                     {
                         Console.WriteLine("Номер не вiдповiдає кiлькостi символiв");
-                        return s;
+                        return s;                        
                     }
                     break;
                 default:
                     Console.WriteLine("Unknown");
                     break;
             }
-          return s;
-
+            return (s);
         }
+      
         static bool IsCreditCardNumberValid(string s)
         {
 
-                string NewNumber = "";
+            string NewNumber = "";
 
-                for (int j = 0; j < s.Length; j++)
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (s[j] != ' ')
                 {
-                    if (s[j] != ' ')
-                    {
-                        NewNumber = NewNumber + s[j];         
-                    }
+                    NewNumber = NewNumber + s[j];
                 }
+            }
             // --------------------------------------------------------
-                if (NewNumber.Length % 2 == 0)
-                {
+            if (NewNumber.Length % 2 == 0)
+            {
                 char[] number = NewNumber.ToCharArray();    // Конвертація строки в масив символів
-                Array.Reverse(number);              // Реверс, для можливості дій над числами з непарним індексом
+                Array.Reverse(number);                      // Реверс, для можливості дій над числами з непарним індексом
                 NewNumber = new string(number);
-                }                          
-                else
-                {
+            }
+            else
+            {
                 char[] number = NewNumber.ToCharArray();
                 NewNumber = new string(number);
-                }
+            }
 
-                int[] mas = new int[23];
-                int i = 0;
-                foreach (int n in NewNumber)
-                {
-                    mas[i] = (n - 48) * (i % 2 == 0 ? 1 : 2);
-                    i++;
-                }
+            int[] mas = new int[23];
+            int i = 0;
+            foreach (int n in NewNumber)
+            {
+                mas[i] = (n - 48) * (i % 2 == 0 ? 1 : 2);
+                i++;
+            }
 
-                int sum = 0;
-                for (int j = 0; j < mas.Length; j++)
-                {
-                    sum += mas[j] / 10 + mas[j] % 10;
-                }
+            int sum = 0;
+            for (int j = 0; j < mas.Length; j++)
+            {
+                sum += mas[j] / 10 + mas[j] % 10;
+            }
 
-                Console.WriteLine(sum);
-                Console.WriteLine("CreditCard is valid {0}", sum % 10 == 0); // Перевірка на кратність 10
+            Console.WriteLine(sum);
+            Console.WriteLine("CreditCard is valid {0}", sum % 10 == 0); // Перевірка на кратність 10
 
-                return sum % 10 == 0;
-            
-        }        
-        static string GenerateNextCreditCardNumber(string s)
-        {
-            string NewCC = "";
-            
+            return sum % 10 == 0;
+
+        }
+        public static string GenerateNextCreditCardNumber(string s)
+        {           
             for (int i = 0; i < s.Length; i++)
             {
-                if (s[i] != ' ')                
+                if (s[i] != ' ')
                 {
                     NewCC = NewCC + s[i];         // Формуванння нового номера кредитної карти на основі переданого з клавіатури
                 }
@@ -312,86 +312,79 @@ namespace CC_3
             long cash = Int64.Parse(NewCC);       // Конвертація строки в число, для можливості інкременту нового номера карти
             pNewCC:                               // Мітка переходу 
             cash++;
-            NewCC = Convert.ToString(cash);       // Конвертація чисда в строку                        
-            Console.WriteLine(NewCC);
+            NewCC = Convert.ToString(cash);       // Конвертація чисда в строку   
+                                 
+            Console.WriteLine(NewCC);                 
+                    
+            if (Perevirka(s) == false)             
+            {
+                if (IsCreditCardNumberValid(NewCC))  // Перевірка валідності нового номера карти
+                {
+                    Console.WriteLine("Next Credit Card is valid {0}", IsCreditCardNumberValid(NewCC));
+                }
+                else
+                    goto pNewCC;                     // Перехід на до мітки для повторення збільшення значення номера карти
+            }
+                        
+            return NewCC;
+        }
 
+        public static bool Perevirka(string s)
+        {
+            
             switch (s[0])
             {
                 case '3':
-                    if ((s[1] == '4') && (NewCC[1] == '5'))
+                    if ((s[1] == '4') && (NewCC[1] == '5')) // Перевірка другого символа вхідної строки "s" та перетвореної "NewCC"
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
+                        ValueBoolPerevirka = ((s[1] == '4') && (NewCC[1] == '5')); //Присврєння bool значенння виразу в дужках змінній "k"
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                        
                     }
                     else if ((s[1] == '7') && (NewCC[1] == '8'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
+                        ValueBoolPerevirka = ((s[1] == '7') && (NewCC[1] == '8'));
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));              
                     }
                     else if ((s[2] == '8') && (NewCC[2] == '9'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
-                    }
-                    else
-                    {
-                        goto SearchNextNumber;
-                    }
+                        ValueBoolPerevirka = ((s[2] == '8') && (NewCC[2] == '9'));
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                        
+                    }                   
                     break;
 
                 case '4':
-                    if ((s[0] == '4') && (NewCC[0] == '5'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
-                    }
-                    else
-                    {
-                        goto SearchNextNumber;
-                    }
+                        if ((s[0] == '4') && (NewCC[0] == '5'))
+                        { 
+                            ValueBoolPerevirka = ((s[0] == '4') && (NewCC[0] == '5'));                            
+                            Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                                                       
+                        }
+                    }                    
                     break;
                 case '5':
                     if ((s[1] == '0') && (NewCC[1] == '1'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
+                        ValueBoolPerevirka = ((s[1] == '0') && (NewCC[1] == '1'));
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                        
                     }
                     else if ((s[1] == '5') && (NewCC[1] == '6'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
-                    }
-                    else
-                    {
-                        goto SearchNextNumber;
-                    }
+                        ValueBoolPerevirka = ((s[1] == '5') && (NewCC[1] == '6'));
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                        
+                    }                    
                     break;
                 case '6':
                     if ((s[0] == '9') && (NewCC[0] == '0'))
                     {
-                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));
-                        return s;
+                        ValueBoolPerevirka = ((s[0] == '9') && (NewCC[0] == '0'));
+                        Console.WriteLine("Введений номер {0} остатнiй \"{1}\" валiдний iз дiапазону вендора", s, IsCreditCardNumberValid(s));                        
                     }
-                    else
-                    {
-                        goto SearchNextNumber;
-                    }
-
                     break;
             }
-
-
-
-            SearchNextNumber:
-
-            if (IsCreditCardNumberValid(NewCC))  // Перевірка валідності нового номера карти
-            {
-                Console.WriteLine("Next Credit Card is valid {0}", IsCreditCardNumberValid(NewCC)); 
-            }
-            else
-                goto pNewCC;                      // Перехід на до мітки для повторення збільшення значення номера карти
-
-            return NewCC;
+        
+            return (ValueBoolPerevirka);
         }
+    
+        
     }
 }
